2920e2f70383bb25d1ddd1390e42531febe7ba80
nickerpasco <37748322+nickerpasco@users.noreply.github.com>
2025-05-10 20:12:32 -0500

    subiendo proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Diseño Rápido', 
+      title: 'Menu Principal', 
       home: const HomeScreen(),
     );
   }
